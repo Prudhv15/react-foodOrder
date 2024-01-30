9aa284eaab4ea6f5115ffe9da366904e8dd4e7d5
prudhvi <jampanaprudvi2@gmail.com>
2024-01-29 21:42:23 -0600

    rendered file in UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import Items from "./components/Items";
 function App() {
   return (
     <>
-      {/* <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-       */}
       <Header />
       <Items />
     </>
